f395e5a86e9d8772cd8261b9fb25d1e7c7ab9acd
AnaliaGiachini <analiagiachini@gmail.com>
2023-11-21 18:57:29 +0100

    general layout of the components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,25 @@
 const About = () => {
   return (
-    <div className="About-container">
-      
+    <div className="About-container" id="About">
+      <h2>Nosotras</h2>
+      <div className="text-center">
+      <img
+          src="https://tecdn.b-cdn.net/img/new/avatars/5.webp"
+          className="mx-auto mb-4 w-32 rounded-lg"
+          alt="Avatar" />
+      <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
+      <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
+      <div className="text-center">
+      <img
+          src="https://tecdn.b-cdn.net/img/new/avatars/5.webp"
+          className="mx-auto mb-4 w-32 rounded-lg"
+          alt="Avatar" />
+      <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
+      <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
+  </div>
+  </div>
     </div>
+    
   )
 }
 export default About;
